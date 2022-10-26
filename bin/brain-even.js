c0701d791a81b9dc23868916937df3a49464ee57
FasTrss <greyhollmen@gmail.com>
2022-10-26 14:33:22 +0300

    add brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,7 @@
-#!/usr/bin/env node
-
 import { sayHi } from "../src/cli.js";
 
 console.log('Welcome to the Brain Games!');
 sayHi();
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
 
