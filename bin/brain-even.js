502ce706b56fac36612e173d9f8cde121dc43df0
FasTrss <greyhollmen@gmail.com>
2022-10-28 10:02:42 +0300

    some changes

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -4,4 +4,9 @@ console.log('Welcome to the Brain Games!');
 sayHi();
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-
+const getRandomIntInclusive = (1, 30) => {
+    min = Math.ceil(1);
+    max = Math.floor(30);
+    const Integer = Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log(Integer);
+}
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -5,3 +5,4 @@ import { sayHi } from "../src/cli.js";
 console.log('Welcome to the Brain Games!');
 sayHi();
 
+
