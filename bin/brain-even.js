806f05f4fecedc0719cdbbaa4bb770faf5975afe
FasTrss <greyhollmen@gmail.com>
2022-10-28 12:12:39 +0300

    fix brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -14,17 +14,19 @@ function getRandomInt(max) {
 
 for (let i = 0; i < 3; i += 1) {
     let num = 0;
-    num = getRandomInt(15);
+    num = getRandomInt(50);
     console.log('Question: ' + num);
     const userAnswer = readlineSync.question('Your answer: ');
     const check1 = (num % 2 === 0 && userAnswer === 'yes');
     const check2 = (num % 2 !== 0 && userAnswer === 'no');
     if ((check1 === true) || (check2 === true)) {
         console.log('Correct!');
+        if (i === 2) {
+            console.log('Congratulations, '+ name + '!');
+        }
     } else {
         const rightAnswer = (num % 2 !== 0 ? 'no' : 'yes')
         console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\n Let's try again, ${name}!`);
         break;
     }
 }
-console.log('Congratulations, '+ name + '!')
