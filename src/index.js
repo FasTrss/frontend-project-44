f7dc1af3c180e395d55db4a843d0a5fa6c426be5
FasTrss <greyhollmen@gmail.com>
2022-11-09 12:30:50 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -16,6 +16,9 @@ export const getRandomIntFromInterval = (minim, maxim) => {
 const roundsCount = 3;
 
 export const launchGame = (description, generateRound) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
   console.log(description);
   for (let i = 0; i < roundsCount; i += 1) {
     const [question, rightAnswer] = generateRound();
